25683a3becf9106616a7b9d091fbbbc9237edde3
Kseniya Tsaruk <kseniya.tsaruk@yandex.ru>
2025-03-15 18:43:04 +0300

    Added cards and delete card

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardElement = cardTemplate.querySelector('.card');
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(cardData, onClickDelete) {
+  const card = cardElement.cloneNode(true);
+  const image = card.querySelector('.card__image');
+  const title = card.querySelector('.card__title');
 
-// @todo: Функция создания карточки
+  image.src = cardData.link;
+  image.alt = cardData.name;
+  title.textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => onClickDelete(card));
 
-// @todo: Вывести карточки на страницу
+  return card;
+}
+
+function deleteCard(card) {
+  card.remove();
+}
+
+initialCards.forEach((item) => {
+  const card = createCard(item, deleteCard);
+  placesList.appendChild(card);
+});
